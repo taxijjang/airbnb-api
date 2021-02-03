57e3a9bb9938f7c19948cabc1726daca1e570ef4
taxijjang <gw9122@naver.com>
2021-02-03 17:13:35 +0900

    1장

---
+++ rooms/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Room
+from users.serializers import TinyUserSerializer
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    user = TinyUserSerializer()
+
+    class Meta:
+        model = Room
+        fields = ['pk', 'name', 'price', 'instant_book', 'user']
+
+
+class BigRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        #exclude = ()
+        fields = '__all__'
