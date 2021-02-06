2e0bfa01a12944d7ac6e3a72060776276ceeb9e0
taxijjang <gw9122@naver.com>
2021-02-06 14:08:31 +0900

    #1-Creating Account

--- users/serializers.py
+++ users/serializers.py
@@ -16,31 +16,32 @@ class RelatedUserSerializer(serializers.ModelSerializer):
         )
 
 
-class ReadUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        exclude = (
-            'groups',
-            'user_permissions',
-            'password',
-            'last_login',
-            'is_superuser',
-            'is_staff',
-            'is_active',
-            'favs',
-            'date_joined',
-        )
+class UserSerializer(serializers.ModelSerializer):
 
-class WriteUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = (
+            "id",
             "username",
             "first_name",
             "last_name",
             "email",
+            "avatar",
+            "superhost",
+            "password",
         )
+
+        read_only_fields = ("id","superhost","avatoar")
 
     def validated_first_name(self, value):
         print(value)
         return value.upper()
+
+    def create(self, validated_data):
+        password = validated_data.get('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -3,23 +3,31 @@ from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ReadUserSerializer, WriteUserSerializer
+from .serializers import UserSerializer
 from .models import User
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 
+class UsersView(APIView):
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            return Response(data=UserSerializer(new_user).data)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(data=ReadUserSerializer(request.user).data)
+        return Response(data=UserSerializer(request.user).data)
 
     def put(self, request):
-        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
         if serializer.is_valid():
             user = serializer.save()
             print(dir(user))
-            return Response()
+            return Response(data=UserSerializer(user).data)
         else:
             return Response(data=serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
@@ -28,7 +36,7 @@ class MeView(APIView):
 def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
-        return Response(data= ReadUserSerializer(user).data)
+        return Response(data= UserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -37,7 +45,7 @@ class FavsView(APIView):
 
     def get(self, request):
         user = request.user
-        serializer = RoomSerializer(user.favs.all(), many=True)
+        serializer = UserSerializer(user.favs.all(), many=True)
         return Response(data=serializer.data)
 
     def put(self, request):
